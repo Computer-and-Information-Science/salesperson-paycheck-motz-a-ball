7b1b794978ebf4908c53e77606698e0cf724a9af
motz-a-ball <memaza98@gmail.com>
2025-09-19 00:33:05 +0000

    Almost correctly coded salesperson data calculator.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,54 @@
+//Matthew Maza
 #include <iostream>
-
+#include <fstream>
+#include <iomanip>
+using namespace std;
 int main() {
-  std::cout << "Hello World!\n";
+  
+ifstream fileDataIn;
+string fileName;
+
+//Prompt for file name input
+cout << "Type file name here ";
+cin >> fileName;
+
+//Declaring variables
+string firstName, lastName;
+double baseSalary, commissionPercent, sales, expenses, grossPay, commission;
+
+//Open file
+fileDataIn.open(fileName);
+
+//Read from file
+fileDataIn >> firstName >> lastName >> baseSalary >> commissionPercent >> sales >> expenses;
+
+//Calculations
+commission = sales*(commissionPercent/100);
+grossPay = baseSalary + commission + expenses;
+
+cout << "Payroll data for " << firstName << " " << lastName << endl;
+cout << endl;
+cout << setprecision(2) << fixed;
+
+cout << "Base Salary:";
+cout << setw(11) << right;
+cout << baseSalary << endl;
+
+cout << "Commission: ";
+cout << setw(11) << right;
+cout << commission << " (" << commissionPercent << "% of " << baseSalary << ")" << endl;
+
+cout << "Expenses: ";
+cout << setw(13) << right;
+cout << expenses << endl;
+
+cout << "               --------" << endl;
+
+cout << "Total:";
+cout << setw(17) << right;
+cout << grossPay << endl;
+
+
+return 0;
+
 }
